a37d676e93224de382c2121083b63fa1700a1796
Dominic Yin <yindongchao@inspur.com>
2020-10-06 10:45:53 +0800

    add message box function to show errors

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
+// +build windows
+
 package main
 
 import (
+	"fmt"
+	"github.com/ydcool/msedge-launcher/pkg"
 	"log"
 	"os"
 	"os/exec"
@@ -13,14 +17,23 @@ const EdgeFlag = "--disable-features=RendererCodeIntegrity"
 func main() {
 	pf86, pf86err := os.Getenv("PROGRAMFILES(x86)"), "%programfiles(x86)%"
 	if pf86 == "" {
-		log.Fatalf("environment variable %s not found", pf86err)
+		_ = pkg.MessageBoxPlain("ErrorLaunchEdge", fmt.Sprintf("environment variable %s not found", pf86err))
+		return
 	}
+
 	edgePath := filepath.Join(pf86, DefaultEdgePath)
+	_, err := os.Stat(edgePath)
+	if err != nil {
+		_ = pkg.MessageBoxPlain("ErrorLaunchEdge", fmt.Sprintf("Failed stat msedge location, Error:\n %v", err))
+		return
+	}
+
 	args := append(os.Args[1:], EdgeFlag)
 	cmd := exec.Command(edgePath, args...)
 	out, err := cmd.CombinedOutput()
 	if err != nil {
-		log.Fatalf("failed excute edge command: %s %v: %v", edgePath, args, err)
+		_ = pkg.MessageBoxPlain("ErrorLaunchEdge", fmt.Sprintf("failed excute edge command:\n %s %v \nError:\n %v", edgePath, args, err))
+		return
 	}
 	log.Print(string(out))
 }
